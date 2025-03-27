3e166cbdb9ddce9eb523749e8433420d045ebf4a
nadeemajayasinghe <nadeemajayasinghe75@gmail.com>
2025-03-27 17:23:00 +0530

    fix27

--- img/script.js
+++ img/script.js
@@ -25,6 +25,7 @@ document.querySelectorAll(".add-to-cart").forEach((button) => {
 
 
 
+
 // Display cart items in the cart page
 const cartItemsContainer = document.getElementById("cart-items");
 if (cartItemsContainer) {
